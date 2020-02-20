73107efde3a503de19a9bdfd1c9f08dd91ee25dd
Erkki Mattila <lasokki@gmail.com>
2020-02-21 00:41:39 +0200

    Add which-key

--- emacs/init.el
+++ emacs/init.el
@@ -29,6 +29,7 @@
 (require 'lsp-mode)
 (require 'groovy-mode)
 (require 'cider)
+(require 'which-key)
 
 ;;; Code:
 (setq load-prefer-newer t)
@@ -41,7 +42,7 @@
 ;;; Commentary:
 
 ;; ## added by OPAM user-setup for Emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
-(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;;(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
 
 (provide 'init)
